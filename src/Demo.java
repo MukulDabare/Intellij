49a90c8ec399aacd79164ebef2884d7c25afd965
MukulDabare <106649658+MukulDabare@users.noreply.github.com>
2024-04-02 10:15:02 +0530

    first

---
+++ src/Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        for(int i = 0; i <=5; i++) {
+          System.out.println(i);
+
+        }
+        
+    }
+
+}
